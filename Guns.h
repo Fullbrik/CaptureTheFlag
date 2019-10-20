27237396b4f120c8f3be400c839e01dae50e011d
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-10-20 14:00:21 -0700

    Added some files

---
+++ Guns.h
@@ -0,0 +1,17 @@
+#define GunCount 2
+
+//Gun values
+const byte GunSprites[GunCount] =
+{
+  0x00, 0xb3
+};
+
+const byte GunProjectileSprites[GunCount] =
+{
+  0x00, 0xb4
+};
+
+const byte GunDamages[GunCount] =
+{
+  0, 1
+};
