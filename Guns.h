b1ff2d5f573e51b773dfca0e7c818473525daef5
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-10-30 15:39:24 -0700

    Remove Mine Placer

--- Guns.h
+++ Guns.h
@@ -1,27 +1,27 @@
-#define GunCount 6
+#define GunCount 5
 
 //Gun values
 const byte GunSprites[GunCount] =
 {
-  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba
+  0x00, 0xb3, 0xb5, 0xb7, 0xb8
 };
 
 const byte GunProjectileSprites[GunCount] =
 {
-  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb
+  0x00, 0xb4, 0xb6, 0xb6, 0xb9
 };
 
 const byte GunDamages[GunCount] =
 {
-  0, 2, 5, 4, 3, 9
+  0, 2, 5, 4, 3
 };
 
 const byte GunSpeeds[GunCount] =
 {
-  0, 1, 5, 3, 1, 0
+  0, 1, 5, 3, 1
 };
 
 const byte GunSpeedMod[GunCount] =
 {
-  6, 6, 7, 6, 9, 5
+  6, 6, 7, 6, 9
 };
