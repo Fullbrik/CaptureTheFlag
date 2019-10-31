8e4cbd33ff60b5935b093bc46524a53cd41a7a58
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-10-31 10:48:41 -0700

    Started new map

--- Guns.h
+++ Guns.h
@@ -4,25 +4,25 @@
 //Gun values
 const byte GunSprites[GunCount] =
 {
-  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba, 0xbc
+  0x00, 0xb3, 0xb5, 0xbc, 0xb7, 0xb8, 0xba, 
 };
 
 const byte GunProjectileSprites[GunCount] =
 {
-  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb, 0xbd
+  0x00, 0xb4, 0xb6, 0xbd, 0xb6, 0xb9, 0xbb,
 };
 
 const byte GunDamages[GunCount] =
 {
-  0, 2, 5, 4, 3, 9, 4
+  0, 2, 5, 4, 4, 3, 9,
 };
 
 const byte GunSpeeds[GunCount] =
 {
-  0, 1, 5, 3, 3, 10, 2
+  0, 1, 5, 2, 3, 3, 10
 };
 
 const byte GunSpeedMod[GunCount] =
 {
-  5, 6, 7, 6, 10, 8, 7
+  5, 6, 7, 7, 6, 10, 8, 
 };
