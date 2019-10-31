fc196228c5c26596fccf85cc95e1bca27865e990
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-10-30 19:34:04 -0700

    Added rocket launcher

--- Guns.h
+++ Guns.h
@@ -1,27 +1,28 @@
-#define GunCount 5
+//Gun number 0 is no gun
+#define GunCount 6
 
 //Gun values
 const byte GunSprites[GunCount] =
 {
-  0x00, 0xb3, 0xb5, 0xb7, 0xb8
+  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba
 };
 
 const byte GunProjectileSprites[GunCount] =
 {
-  0x00, 0xb4, 0xb6, 0xb6, 0xb9
+  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb
 };
 
 const byte GunDamages[GunCount] =
 {
-  0, 2, 5, 4, 3
+  0, 2, 5, 4, 3, 9
 };
 
 const byte GunSpeeds[GunCount] =
 {
-  0, 1, 5, 3, 1
+  0, 1, 5, 3, 1, 10
 };
 
 const byte GunSpeedMod[GunCount] =
 {
-  6, 6, 7, 6, 9
+  5, 6, 7, 6, 9, 8
 };
