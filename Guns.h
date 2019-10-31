bbb35fa5306ab9a32428f5e838a5d3784849cc3b
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-10-31 10:41:43 -0700

    Added new gun

--- Guns.h
+++ Guns.h
@@ -1,28 +1,28 @@
 //Gun number 0 is no gun
-#define GunCount 6
+#define GunCount 7
 
 //Gun values
 const byte GunSprites[GunCount] =
 {
-  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba
+  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba, 0xbc
 };
 
 const byte GunProjectileSprites[GunCount] =
 {
-  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb
+  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb, 0xbd
 };
 
 const byte GunDamages[GunCount] =
 {
-  0, 2, 5, 4, 3, 9
+  0, 2, 5, 4, 3, 9, 4
 };
 
 const byte GunSpeeds[GunCount] =
 {
-  0, 1, 5, 3, 1, 10
+  0, 1, 5, 3, 3, 10, 2
 };
 
 const byte GunSpeedMod[GunCount] =
 {
-  5, 6, 7, 6, 9, 8
+  5, 6, 7, 6, 10, 8, 7
 };
