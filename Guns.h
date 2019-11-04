6c5895626ec23a0302224ca7c269be7bc2b730cf
Fullbrik <40151628+Fullbrik@users.noreply.github.com>
2019-11-04 15:22:39 -0800

    Fixed gun

--- Guns.h
+++ Guns.h
@@ -4,25 +4,25 @@
 //Gun values
 const byte GunSprites[GunCount] =
 {
-  0x00, 0xb3, 0xb5, 0xbc, 0xb7, 0xb8, 0xba, 
+  0x00, 0xb3, 0xb5, 0xb7, 0xb8, 0xba, 0xbc,
 };
 
 const byte GunProjectileSprites[GunCount] =
 {
-  0x00, 0xb4, 0xb6, 0xbd, 0xb6, 0xb9, 0xbb,
+  0x00, 0xb4, 0xb6, 0xb6, 0xb9, 0xbb, 0xbd,
 };
 
 const byte GunDamages[GunCount] =
 {
-  0, 2, 5, 4, 4, 3, 9,
+  0, 2, 5, 4, 3, 9, 6,
 };
 
 const byte GunSpeeds[GunCount] =
 {
-  0, 1, 5, 2, 3, 3, 10
+  0, 1, 5, 3, 3, 10, 2
 };
 
 const byte GunSpeedMod[GunCount] =
 {
-  5, 6, 7, 7, 6, 10, 8, 
+  5, 6, 7, 6, 10, 8, 7,
 };
